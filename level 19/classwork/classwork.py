7c4a28fda788c4873300877959f98cbef7e1b437
datagogila <datagogila.@gmail.com>
2024-12-16 22:30:51 +0400

    first commit

---
+++ level 19/classwork/classwork.py
@@ -0,0 +1,13 @@
+#what is the output of this code
+nums = [1,2,3,4,5,6]
+res = nums[::-1]
+print(res[2])
+#4
+
+#how many elements will this slice return if the "names" list contains 5 items?
+
+#names[1:-1]
+
+#4
+
+
